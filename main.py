827b433da85835df4987dc9e5fff384072052ad8
baristds <101243366+baristds@users.noreply.github.com>
2023-11-17 12:03:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -98,4 +98,5 @@ def upload_to_s3(file_paths, bucket_name):
     return uploaded_data, upload_speed
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
+    
